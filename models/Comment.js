664ef739c0f3f078d2121eeca4c8e25c22c98e59
Ruben Matamoros <ruben@valiantcreative.net>
2021-03-06 01:11:23 -0500

    created comment functionality

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// create the Comment model using the CommentSchema
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
